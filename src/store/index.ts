3ac61e2b0016fbac1f9218aa179df5635c833e45
Davi Right Here <hoanghienle69@gmail.com>
2024-12-08 20:58:06 +0700

    update Store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,30 @@
 // src/store/store.ts
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './slices';
+import { combinedApi } from '../api/index';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
-    rootReducer,
+    [combinedApi.authApi.reducerPath]: combinedApi.authApi.reducer,
+    [combinedApi.chatApi.reducerPath]: combinedApi.chatApi.reducer,
+    [combinedApi.propertyApi.reducerPath]: combinedApi.propertyApi.reducer,
+    [combinedApi.residentApi.reducerPath]: combinedApi.residentApi.reducer,
+    [combinedApi.serviceApi.reducerPath]: combinedApi.serviceApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(combinedApi.authApi.middleware)
+      .concat(combinedApi.chatApi.middleware)
+      .concat(combinedApi.propertyApi.middleware)
+      .concat(combinedApi.residentApi.middleware)
+      .concat(combinedApi.serviceApi.middleware),
+  devTools: process.env.NODE_ENV !== 'production', // Optional: disable devtools in production
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// Optional: Export hooks for typing
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
